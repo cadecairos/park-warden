2786f3766d73f08586bd6fb01b1543f3853afaad
Christopher De Cairos <cade@mozillafoundation.org>
2014-04-14 15:18:33 -0400

    use multi command

--- server.js
+++ server.js
@@ -54,23 +54,29 @@ server.on("request", function(req, res) {
       return res.end(JSON.stringify({error: err.toString()}));
     }
 
+    user_ids = user_ids.filter(function(key) {
+      return /^\d+(?!contribution)$/.test(key);
+    });
+
     var one_year_ago = queryDate.valueOf() - (365 * 24 * 60 * 60 * 1000);
     var total_active_contributors = 0;
     var seven_days_ago = queryDate.valueOf() - (7 * 24 * 60 * 60 * 1000);
     var new_contributors_7_days = 0;
 
     async.doUntil(function fn(cb) {
-      var key = user_ids.shift();
-      if (/^\d+(?!contribution)$/.test(key) ) {
-        redis_client.hmget([key, "firstContribution"], function(err, data) {
-          if (err) {
-            return cb(err);
-          }
-          redis_client.smembers(key + ":contributions", function(err, setData) {
-            if (err) {
-              return cb(err);
-            }
+      var keys = user_ids.splice(0, 1000);
 
+      keys.map(function(key) {
+        return ["hmget", key, "firstContribution"];
+      });
+
+      var multi = redis_client.multi(keys).exec(function(err, replies) {
+        if (err) {
+          return cb(err);
+        }
+
+        async.eachSeries(replies, function(data, done) {
+          redis_client.smembers(data[0] + ":contributions", function(err, setData) {
             var latestContribution;
 
             setData.forEach(function(date) {
@@ -83,7 +89,7 @@ server.on("request", function(req, res) {
               }
             });
 
-            var firstContribution = new Date(data[0]).valueOf();
+            var firstContribution = new Date(data[1]).valueOf();
 
             // This will introduce errors over time. As latest contribution date will move ahead of query dates for historic data.
             if (!isNaN(latestContribution) && (latestContribution > one_year_ago) && (latestContribution < queryDate)) {
@@ -92,13 +98,11 @@ server.on("request", function(req, res) {
             if (!isNaN(firstContribution) && (firstContribution > seven_days_ago) && (latestContribution < queryDate)) {
               new_contributors_7_days++;
             }
-            cb();
+
+            done();
           });
-        });
-      } else {
-        // ignore this key
-        cb();
-      }
+        }, cb);
+      });
     }, function test() {
       return user_ids.length === 0;
     }, function done(err) {
